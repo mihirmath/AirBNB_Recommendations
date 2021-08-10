a2784fe775a08f1d82c1c3e362a7c43e99367463
Mihir Mathur <64558607+mihirmath@users.noreply.github.com>
2021-08-10 15:07:22 -0700

    Update load.py

--- data_loading/load.py
+++ data_loading/load.py
@@ -15,8 +15,13 @@ response = amadeus.shopping.flight_offers_search.get(
             departureDate='2021-08-10',
             adults='1')
 
-url = "neo4j+s://e54715b3.databases.neo4j.io:7687"
-driver = GraphDatabase.driver(url, auth=("neo4j", "d6xX8PrwU_0UMPhqAy76MMMiuAtzJqF6_djE3TnliO0"))
+#Cloud Database
+#url = "neo4j+s://e54715b3.databases.neo4j.io:7687"
+#driver = GraphDatabase.driver(url, auth=("neo4j", "d6xX8PrwU_0UMPhqAy76MMMiuAtzJqF6_djE3TnliO0"))
+
+#Local Database
+url = "bolt://localhost:7687"
+driver = GraphDatabase.driver(url, auth=("neo4j", "neo4j123"))
 
 #Convert Euros to US Dollar, round to 2 decimals
 def usd(amount):
@@ -39,7 +44,7 @@ def parseDuration(duration):
 #Neo4j Python Driver
 def merge_flight_node(tx, id, flightNumber, oneWay, grandTotal, duration, departure_iataCode, departure_airportName, 
                       arrival_iataCode, arrival_airportName, departure_cityName, arrival_cityName, departure_countryName, 
-                      arrival_countryName, carrierCode, airlineName, departureTime, arrivalTime):
+                      arrival_countryName, carrierCode, airlineName, departureTime, arrivalTime, nextFlightID):
     #Query
     tx.run('MERGE (Flight:Flight {id: $id, flightNumber: $flightNumber, oneWay: $oneWay, grandTotal: $grandTotal, duration: $duration})'
            'MERGE (departureAirport:Airport {iataCode: $departure_iataCode, name: $departure_airportName})'
@@ -55,7 +60,9 @@ def merge_flight_node(tx, id, flightNumber, oneWay, grandTotal, duration, depart
            'MERGE (arrivalAirport)-[:IN_CITY]->(arrivalCity)'
            'MERGE (departureCity)-[:IN_COUNTRY]->(departureCountry)'
            'MERGE (arrivalCity)-[:IN_COUNTRY]->(arrivalCountry)'
-           'MERGE (Flight)-[:HAS_AIRLINE]->(Airline)', 
+           'MERGE (Flight)-[:HAS_AIRLINE]->(Airline)'
+           #'MATCH (n:Flight) WHERE n.id = $nextFlightID'
+           'MERGE (Flight)-[:NEXT_FLIGHT]->(n:Flight) ON MATCH SET n.id = $nextFlightID', 
                                                 id=id, 
                                                 flightNumber=flightNumber,
                                                 oneWay=oneWay,
@@ -72,7 +79,8 @@ def merge_flight_node(tx, id, flightNumber, oneWay, grandTotal, duration, depart
                                                 carrierCode=carrierCode,
                                                 airlineName=airlineName,
                                                 departureTime=departureTime,
-                                                arrivalTime=arrivalTime)
+                                                arrivalTime=arrivalTime,
+                                                nextFlightID=nextFlightID)
 #Open airport csv file
 with open ('data_loading/airports.csv') as iataFile:
     reader = csv.DictReader(iataFile)
@@ -107,6 +115,10 @@ for a, flight in enumerate(response.data):
         for ii, segment in enumerate(itinerary['segments']):
             id = int(segment['id'])
             flightNumber = segment['carrierCode'] + segment['number']
+            if ii > 0:
+                nextFlightID = id
+            else:
+                nextFlightID = 1000000
             duration = parseDuration(segment['duration'])
             departure_iataCode = segment['departure']['iataCode']
             departureTime = segment['departure']['at']
@@ -133,6 +145,6 @@ for a, flight in enumerate(response.data):
                                                    departure_iataCode, departure_airportName, arrival_iataCode, 
                                                    arrival_airportName, departure_cityName, arrival_cityName, 
                                                    departure_countryName, arrival_countryName, carrierCode, airlineName, 
-                                                   departureTime, arrivalTime)
+                                                   departureTime, arrivalTime, nextFlightID)
 #Closes the Neo4j Python Driver
 driver.close()
